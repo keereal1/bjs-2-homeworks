0095e053ee5610b194c2187630759046a0c61beb
krlo <kirill.osadchenko@gmail.com>
2024-05-12 15:35:40 +0500

    Оптимизация кода

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let d = b ** 2 - 4 * a * c;
   let arr = [];
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+   if (d === 0) {
     arr = [-b / (2 * a)];
   } else if (d > 0) {
     arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
@@ -18,7 +16,6 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let monthPercent = (percent / 100) / 12;
   let creditBody = amount - contribution;
   let monthlyPay = creditBody * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
-  let totalPayment = parseFloat((monthlyPay * countMonths).toFixed(2));
-  
-  return totalPayment;
+
+  return parseFloat((monthlyPay * countMonths).toFixed(2));
 }
